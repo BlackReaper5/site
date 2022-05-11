848927ddfe1ca3b3809888229d9be3964d616115
Abid <thecaptainabid@gmail.com>
2022-05-11 18:42:32 +0200

    Add player with controls

--- main.js
+++ main.js
@@ -1,17 +1,37 @@
 import * as THREE from "https://unpkg.com/three@0.127.0/build/three.module.js"
 import { OrbitControls } from "https://unpkg.com/three@0.127.0/examples/jsm/controls/OrbitControls.js";
 
-let camera, controls, scene, renderer;
-const fov = 75;
+let camera, controls, scene, renderer, player, forward_button, backward_button;
+let cube, cube2;
+const fov = 100;
 const aspect = window.innerWidth / window.innerHeight;
 const near = 1;
 const far = 1000;
 
-init();
-animate();
+const clock = new THREE.Clock();
+const speed = 2;
 
-function init() {
+initWorld();
 
+function initWorld() {
+  // Buttons
+  forward_button = document.getElementById("forward_button");
+  forward_button.addEventListener("click", function () {
+
+    onForward();
+
+  });
+
+  backward_button = document.getElementById("backward_button");
+  backward_button.addEventListener("click", function () {
+
+    onBackward();
+
+  });
+
+
+
+  // Main 
   scene = new THREE.Scene();
 
   renderer = new THREE.WebGLRenderer({
@@ -21,70 +41,79 @@ function init() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 
   camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
-  camera.position.z = 500;
 
-  // controls
 
+
+  // Controls
   controls = new OrbitControls(camera, renderer.domElement);
   controls.listenToKeyEvents(window); // optional
-
   //controls.addEventListener( 'change', render ); // call this only in static scenes (i.e., if there is no animation loop)
-
   controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
   controls.dampingFactor = 0.05;
-
   controls.screenSpacePanning = false;
-
-  controls.minDistance = 100;
-  controls.maxDistance = 400;
-
+  // controls.minDistance = 100;
+  // controls.maxDistance = 400;
   controls.maxPolarAngle = Math.PI / 2;
 
-  // Object
+
+
+  // Objects
   const geometry = new THREE.BoxGeometry(10, 10, 10);
   const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
-  const cube = new THREE.Mesh(geometry, material);
-  const cube2 = new THREE.Mesh(geometry, material);
-  cube.position.set(100, 0, 0);
-  cube2.position.set(0, 0, 250);
+  cube = new THREE.Mesh(geometry, material);
+  cube2 = new THREE.Mesh(geometry, material);
+  const cubePosition = new THREE.Vector3(0, 0, 0);
+  const cube2Position = new THREE.Vector3(250, 0, -300);
+  cube.position.set(cubePosition.x, cubePosition.y, cubePosition.z);
+  cube2.position.set(cube2Position.x, cube2Position.y, cube2Position.z);
 
   scene.add(cube, cube2);
-  // lights
+
+
+
+  // Lights
   const ambientLight = new THREE.AmbientLight(0xffffff);
   scene.add(ambientLight);
+
 
   // Listeners
   window.addEventListener('resize', onWindowResize);
   // window.addEventListener("wheel", onMouseWheel);
+
+
+
+  // Helpers
+  const size = 1000;
+  const divisions = 50;
+
+  const gridHelper = new THREE.GridHelper(size, divisions);
+  scene.add(gridHelper);
+
+
+
+  // Inits
+  initPlayer();
+  update();
 }
 
 // Helper functions
+// Buttons
+
+function onForward() {
+  console.log("Go forward")
+  player.translateZ(10);
+}
+
+function onBackward() {
+  console.log("Go backward")
+}
+
 function render() {
   if (resizeRendererToDisplaySize(renderer)) {
     const canvas = renderer.domElement;
     camera.aspect = canvas.clientWidth / canvas.clientHeight;
     camera.updateProjectionMatrix();
   }
-  
-  renderer.render(scene, camera);
-
-}
-
-function animate() {
-
-  requestAnimationFrame(animate);
-
-  controls.update(); // only required if controls.enableDamping = true, or if controls.autoRotate = true
-
-  render();
-
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-
-  renderer.setSize(window.innerWidth, window.innerHeight);
   renderer.render(scene, camera);
 }
 
@@ -99,8 +128,51 @@ function resizeRendererToDisplaySize(renderer) {
   return needResize;
 }
 
-function onMouseWheel() {
-  const top = document.body.getBoundingClientRect().top;
-  camera.position.z = top * -0.01;
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
+}
+
+function followCamera() {
+  //Offset from camera to player
+  var relativeCameraOffset = new THREE.Vector3(0, 5, 10);
+
+  //UPDATE PLAYER WORLD MATRIX FOR PERFECT CAMERA FOLLOW
+  player.updateMatrixWorld()
+  //Apply offset to player matrix
+  var cameraOffset = relativeCameraOffset.applyMatrix4(player.matrixWorld);
+
+  //Apply position offset to camera DIRECTLY -> NOT SMOOTH
+  // camera.position.x = cameraOffset.x;
+  // camera.position.y = cameraOffset.y;
+  // camera.position.z = cameraOffset.z;
+
+  //SMOOTH CAMERA POSITION TO TARGET POSITION
+  camera.position.lerp(cameraOffset, 0.1);
+  camera.lookAt(player.position);
+}
+
+function initPlayer() {
+  //Create player object
+  const geometry = new THREE.BoxGeometry(2, 5, 5);
+  const material = new THREE.MeshStandardMaterial({ color: 0xff6347 });
+  player = new THREE.Mesh(geometry, material);
+  player.position.set(0, 0, 300);
+  player.position.needsUpdate = true;
+
+  // add the object to the scene
+  scene.add(player);
+}
+
+function update(time) {
+  //UPDATE CAMERA POSITION
+  followCamera();
+
+  //UPDATE RENDER
+  render();
   controls.update();
+  requestAnimationFrame(update);
 }
